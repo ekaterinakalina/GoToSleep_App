4617b212cb1ccea1ec5bb415855eefdb06dfc9f8
Ekaterina Kalina <kalina.katharina@gmail.com>
2023-08-31 10:07:11 +0300

    Initial Commit

---
+++ GoToSleep/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GoToSleep
+//
+//  Created by Ekaterina Kalina on 31.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoToSleep/GoToSleepApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoToSleepApp.swift
+//  GoToSleep
+//
+//  Created by Ekaterina Kalina on 31.08.23.
+//
+
+import SwiftUI
+
+@main
+struct GoToSleepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
